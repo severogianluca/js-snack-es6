fa6fc8b3574c1953ddf357acd7cf4df9eccecabe
severogianluca <gianlucasevero300@gmail.com>
2025-03-07 16:49:58 +0100

    Feat: create snak1

---
+++ snack1/js/app.js
@@ -0,0 +1,8 @@
+const biciclette = [
+    {nome : 'bmx', peso : '10 Kg'},
+    {nome : 'montainByke', peso : '15 kg'},
+    {nome : 'classicByke', peso : '8 kg'}
+]
+console.log(biciclette);
+console.log(biciclette[2].nome);
+
